0e7d90ed6ee79ce6c7e21dda0b0b7a9c0d2cac37
Tensorflow-Ranger <58466090+Tensorflow-Ranger@users.noreply.github.com>
2021-06-29 00:35:31 +0530

    added class notes maker!!!!!

--- main.py
+++ main.py
@@ -12,6 +12,21 @@ from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
 from gensim.summarization import summarize
 import re
+import requests
+import os
+import moviepy.editor as mp
+from sumy.parsers.plaintext import PlaintextParser #We're choosing a plaintext parser here, other parsers available for HTML etc.
+from sumy.nlp.tokenizers import Tokenizer 
+from sumy.summarizers.lsa import LsaSummarizer
+import speech_recognition as sr
+from punctuator import Punctuator
+import glob
+from pydub import AudioSegment
+
+p = Punctuator('.\\files\\INTERSPEECH-T-BRNN.pcl')
+r = sr.Recognizer()
+summarizer_lsa = LsaSummarizer()
+headers = {'user-agent': 'Wget/1.16 (linux-gnu)'}
 
 firebaseConfig = {
     "apiKey": "AIzaSyDw65BvUcaILJoXX8Mws5QOTRLjyXCGlf0",
@@ -55,26 +70,63 @@ def upload():
     f = request.files.get('file')
     f.save(secure_filename("f.txt"))
     output_string = StringIO()
-    with open(secure_filename("f.txt"), 'rb') as in_file:
-        parser = PDFParser(in_file)
-        doc = PDFDocument(parser)
-        rsrcmgr = PDFResourceManager()
-        device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        for page in PDFPage.create_pages(doc):
-            interpreter.process_page(page)
+    with open(secure_filename(".\\files\\f.txt"), 'rb') as in_file:
+      parser = PDFParser(in_file)
+      doc = PDFDocument(parser)
+      rsrcmgr = PDFResourceManager()
+      device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+      interpreter = PDFPageInterpreter(rsrcmgr, device)
+      for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+      document = output_string.getvalue()
+      document = document.replace("/n", "")
+      return render_template("Summary.html", chapter_summary = summarize(document, ratio = 0.3))
 
-    document = output_string.getvalue()
-    document = document.replace("/n", "")
-    return render_template("Summary.html", chapter_summary = summarize(document, ratio = 0.1))
-    
+@app.route("/classnotesmaker", methods = ["GET", "POST"])
+def classnotes():
+  req = requests.get(request.form.get("dropbox"), stream=True, headers=headers, allow_redirects=True)
+  open('.\\files\\video.mp4', 'wb').write(req.content)
+  files = glob.glob('.\\chunks\\*')
+  for f in files:
+      os.remove(f)
+  my_clip = mp.VideoFileClip(".\\files\\video.mp4")
+  my_clip.audio.write_audiofile(".\\files\\audio_from_video.wav")
+  audio = AudioSegment.from_wav(".\\files\\audio_from_video.wav")
+  duration = audio.duration_seconds
+  t1 = 0 * 1000
+  t2 = 59 * 1000
+  while duration > 5:
+      newAudio = audio[t1:t2]
+      newAudio.export('.\\chunks\\newSong' + str(t1/1000) + '.wav', format="wav")
+      t1 += 60 * 1000
+      t2 += 60 * 1000
+      duration -= 60
+  full_text = ""
+  for filename in os.listdir('.\\chunks'):
+      with sr.AudioFile('.\\chunks\\'+filename) as source:
+          audio = r.record(source)  
+          try:
+              text = p.punctuate(r.recognize_google(audio))
+              full_text += " " + text 
+          except sr.UnknownValueError:
+              print("Google Speech Recognition could not understand audio")
+              break
+          except sr.RequestError as e:
+              print("Could not request results from Google Speech Recognition service; {0}".format(e))
+              break
+  count = full_text.count(".")
+  noofsent = round(30/100 * count)
+  parser = PlaintextParser.from_string(full_text,Tokenizer("english"))
+  summary_2 =summarizer_lsa(parser.document,noofsent) 
+  summary = ""
+  for sentence in summary_2:
+      summary += str(sentence)
+  return render_template("Summary.html", class_summary=summary)
 
 @app.route("/summarizer", methods = ["GET", "POST"])
 def summarizer():
   return render_template("Summary.html")
 
-			
-			
 @app.route('/', methods=['GET', 'POST'])
 def login_page():
 	return render_template('login.html')
