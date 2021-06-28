78a3ec0ca32e27098d62ced0ce92c3749147cc9f
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2021-06-28 14:32:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import urllib
 import json
 from flask import *
+from firebase import Firebase
 
+firebaseConfig = {
+    "apiKey": "AIzaSyDw65BvUcaILJoXX8Mws5QOTRLjyXCGlf0",
+    "authDomain": "webschool-7e9db.firebaseapp.com",
+    "projectId": "webschool-7e9db",
+    "storageBucket": "webschool-7e9db.appspot.com",
+    "messagingSenderId": "396594842645",
+    "appId": "1:396594842645:web:df2c845b47405bacb9521c",
+    "databaseURL" : " "
+  }
 data = {"rahul.jaikrishna@gmail.com" : "hi", "alvinbengeorge@gmail.com": "hi"}
 app = Flask(__name__)
+fire = Firebase(firebaseConfig)
+auth = fire.auth()
 
 @app.route('/login', methods=['GET', 'POST'])
 def basic():
-	if request.method == 'POST':
-		email = request.form.get("sup")
-		password = request.form.get("pwd")
-		print(type(password), password, email, type(email))
-		for each in data:
-			if email == each:
-				if password == data[each]:
-					return render_template('Intro_page.html')
-					break
-				else:
-					return "wrong pass"
-			else:
-				"wrong user"
+    if request.method == 'POST':
+        email = request.form.get("sup")
+        password = request.form.get("pwd")
+    try:
+      login = auth.sign_in_with_email_and_password(email, password)
+      return render_template('Intro_page.html')
+    except:
+      return "Wrong Email and Password"
 			
 			
 					
