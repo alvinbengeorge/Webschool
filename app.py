51ca2c78c5ffe0a4e6138b8ed4cdf58c3eff8362
PythonicNoob <atulsyan850@gmail.com>
2021-06-30 10:12:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -165,5 +165,7 @@ def about():
 def intro():
   return render_template('Intro_page.html')
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+  port = int(os.environ.get("PORT", 5000))
+  print(f"Running on: 127.0.0.1:{port}")
+  app.run(host='127.0.0.1', port=port, threaded=True)
